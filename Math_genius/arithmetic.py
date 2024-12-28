4a43255adb1f4d49de74d5d6362f6861df5b78eb
AlvaroB12 <alvaritokarapitos@gmail.com>
2024-12-28 20:14:32 +0100

    Update arithmetic.py

--- Math_genius/arithmetic.py
+++ Math_genius/arithmetic.py
@@ -1,1 +1,9 @@
-d
+class Main():
+  class Mates():
+    class Aritmetica():
+      def Sumar(a, b):
+        return a + b
+input1 = 'Ingrese el 1er número'
+input2 = 'Ingrese el 2do número'
+Sumar = Main.Mates.Aritmetica.Sumar(float(input(input1)), float(input(input2)))
+print(f"El resultado es {Sumar}.")
