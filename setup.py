3ab4c3f2faa7354a7b38749c4e47d9dd7738cd4f
AlvaroB12 <alvaritokarapitos@gmail.com>
2024-12-31 20:18:20 +0100

    Actualizar setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="Math_genius",
+    version="0.1.0",
+    author="Álvaro Bravo López",
+    author_email="alvaritobralo@gmail.com",
+    description="Math_genius is a small  python library ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AlvaroB12/Math_genius",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
